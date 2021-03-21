b6df6597f368751fdd61c0c226df84139c4491cf
mariamodat <mariamodat0@gmail.com>
2021-03-21 13:13:38 +0200

    base

---
+++ js/apps.js
@@ -0,0 +1,17 @@
+'use strict';
+const seattle = {
+
+  AvgCookiesSale : 6.3
+
+
+
+
+
+};
+
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
